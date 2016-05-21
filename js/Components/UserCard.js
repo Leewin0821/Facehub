653a72c63cd76427d83c9d416bc18ba2ff0f157d
lwzhang <lwzhang@thoughtworks.com>
2016-05-21 17:57:15 +0800

    Revert "[Leewin] Added office tag for user card"
    This reverts commit e3d3dd9fb0370367c82682ce5b4367bc1ce0e345.

--- js/Components/UserCard.js
+++ js/Components/UserCard.js
@@ -13,12 +13,7 @@ export default class UserCard extends Component {
         <Image style={style.avatar}
         source={{uri: 'http://www.bmwusa.com/bmw/api/assets/images/BMWi/BMWi8/BMWi_i8_module4_B4.jpg?v=5fc09bfa5efbb09cf3cd0e73e2d9aad7'}}/>
         <View style={style.textWrapper}>
-          <View style={style.nameContainer}>
-            <Text style={style.name}>Leewin</Text>
-            <View style={style.tagContainer}>
-              <Text style={style.officeTag}>New York</Text>
-            </View>
-          </View>
+          <Text style={style.name}>Leewin</Text>
           <Text style={style.job}>Boss</Text>
           <Text style={style.email}>lwzhang@thoughtworks.com</Text>
         </View>
@@ -29,10 +24,9 @@ export default class UserCard extends Component {
 
 const style = StyleSheet.create({
   name: {
-    fontSize: 18,
+    fontSize: 10,
     color: '#333',
     fontWeight: 'bold',
-    flex: 1,
   },
   job: {
     fontSize: 14,
@@ -70,21 +64,5 @@ const style = StyleSheet.create({
   avatar: {
     width: 73,
     height: 73,
-  },
-  nameContainer: {
-    flexDirection: 'row',
-  },
-  tagContainer: {
-    flex: 2,
-    alignSelf: 'center'
-  },
-  officeTag: {
-    fontSize: 10,
-    alignSelf: 'flex-start',
-    backgroundColor: '#F50057',
-    color: 'white',
-    borderRadius: 9,
-    padding: 4,
-    textAlign: 'center',
-  },
-});
+  }
+})
