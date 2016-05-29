855309cbaab05afe8d8db8b3759c54a63b2b04b8
lwzhang <lwzhang@thoughtworks.com>
2016-05-29 12:54:10 +0800

    [Leewin] Introduced Redux

--- js/Actoins/UserActions.js
+++
@@ -1,3 +0,0 @@
-var UserActions = React.createActions({
-  'getUserData'
-});
--- js/Components/UserCard.js
+++ js/Components/UserCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Reflux from 'reflux';
 import {
   View,
   StyleSheet,
@@ -8,27 +7,24 @@ import {
 } from 'react-native';
 
 export default class UserCard extends Component {
-  // mixins: [Reflux.listenTo(UserStore, 'onStatusChange')],
-  // getInitialState: function() {
-  //   return {users: []};
-  // },
-  // onStatusChange: function() {
-  //   this.setState(users: UserStore.users);
-  // },
+  componentWillMount() {
+    this.user = this.props.user;
+  }
+
   render() {
     return (
       <View style={[style.card, style.shadow]}>
         <Image style={style.avatar}
-        source={{uri: 'http://www.bmwusa.com/bmw/api/assets/images/BMWi/BMWi8/BMWi_i8_module4_B4.jpg?v=5fc09bfa5efbb09cf3cd0e73e2d9aad7'}}/>
+        source={{uri:this.user.photo}}/>
         <View style={style.textWrapper}>
           <View style={style.nameContainer}>
-            <Text style={style.name}>Leewin</Text>
+            <Text style={style.name}>{this.user.name}</Text>
             <View style={style.tagContainer}>
-              <Text style={style.officeTag}>New York</Text>
+              <Text style={style.officeTag}>{this.user.office}</Text>
             </View>
           </View>
-          <Text style={style.job}>Boss</Text>
-          <Text style={style.email}>lwzhang@thoughtworks.com</Text>
+          <Text style={style.job}>{this.user.title} on {this.user.project}</Text>
+          <Text style={style.email}>{this.user.email}</Text>
         </View>
       </View>
     );
