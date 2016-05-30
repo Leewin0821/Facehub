5a463f882992d4c0e0ec49edb0b1d63e1a785ee8
lwzhang <lwzhang@thoughtworks.com>
2016-05-30 17:31:54 +0800

    [Leewin] Changed example info

--- js/Actions/userListAction.js
+++ js/Actions/userListAction.js
@@ -1,27 +1,27 @@
 const mockUserlist = [
    {
-     "name": "CC",
+     "name": "Iron Man",
       "office": "Hot",
-      "title": "HR",
-      "photo": "http://7xr6pw.com1.z0.glb.clouddn.com/5fb969f7e4354c92bb0276199754de38.png",
+      "title": "Rich",
+      "photo": "https://upload.wikimedia.org/wikipedia/en/e/e0/Iron_Man_bleeding_edge.jpg",
       "created_at": "2016-03-04 15:38:48",
       "id": 1499,
-      "project": "Hot HR",
-      "avatar": "http://7xr6pw.com1.z0.glb.clouddn.com/76cb804727c9596df57dbaddd41c3599.png",
+      "project": "S.H.I.E.L.D",
+      "avatar": "https://upload.wikimedia.org/wikipedia/en/e/e0/Iron_Man_bleeding_edge.jpg",
       "onboard": "2014-12-04",
-      "email": "cc@sample.com"
+      "email": "ironman@sample.com"
     },
     {
-      "name": "LG",
+      "name": "Captain",
       "office": "Old",
-      "title": "Dev",
-      "photo": "http://7xr6pw.com1.z0.glb.clouddn.com/f9885c0f83ed4420b44f61421f04e664.png",
+      "title": "Undercover",
+      "photo": "http://i1.wp.com/cdn.bgr.com/2014/04/captain-america.jpg?w=625",
       "created_at": "2016-03-04 15:57:00",
       "id": 1500,
-      "project": "Water",
-      "avatar": "http://7xr6pw.com1.z0.glb.clouddn.com/e877be36603353167125fa37060ea80f.png",
+      "project": "Hydra",
+      "avatar": "http://i1.wp.com/cdn.bgr.com/2014/04/captain-america.jpg?w=625",
       "onboard": "2015-07-06",
-      "email": "lg@sample.com"
+      "email": "captain@sample.com"
     }
 ];
 
